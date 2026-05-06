8c2f007eea6205afd6efbdb5b4de40819119a2f9
Mostafa Hatem <mostafahatemghonem@gmail.com>
2026-05-06 16:15:47 +0300

    Optimize data handling and update plot logic
    Refactor data handling and improve performance by using deque for buffering. Comment out print statements to enhance data processing speed.

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,4 +1,8 @@
 import sys
+
+import queue
+from collections import deque
+
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLabel
 from PyQt5.QtCore import QTimer, Qt
 from PyQt5.QtGui import QFont
@@ -44,15 +48,14 @@ class Visualizer(QMainWindow):
         
        
         self.ins_line = self.graph.plot(pen=pg.mkPen(color='#FF00FF', width=2), name="INS ")
-        
-        
         self.gps_line = self.graph.plot(pen=pg.mkPen(color='#00D4FF', width=1, style=Qt.DotLine), name="GPS ")
-        
-        
         self.kal_line = self.graph.plot(pen=pg.mkPen(color='#39FF14', width=3), name="Kalman Filter ")
 
+        # self.times, self.gps_vals, self.ins_vals, self.kal_vals = [], [], [], []
         
-        self.times, self.gps_vals, self.ins_vals, self.kal_vals = [], [], [], []
+        #عشان يكون اسرع بس مش مشكله في الكود هتلاحظي هنا اني برضو خليت ال condition اللي في ال while تعليق عشان برضو مياخرش بس سرشي عنها 
+        BUFFER_SIZE = 200
+        self.times, self.gps_vals, self.ins_vals, self.kal_vals = deque(maxlen=BUFFER_SIZE), deque(maxlen=BUFFER_SIZE), deque(maxlen=BUFFER_SIZE), deque(maxlen=BUFFER_SIZE)
 
         # التايمر ( سريع وسلس)
         self.timer = QTimer()
@@ -60,11 +63,14 @@ class Visualizer(QMainWindow):
         self.timer.start(30)
 
     def update_plot(self):
+        new_date = False
         # سحب البيانات من طابور مصطفى بدون تعديل في المنطق
         while not self.ui_queue.empty():
             try:
                 t, gps, ins, kal = self.ui_queue.get_nowait()
-                print(f"Data from mostafa -> GPS : {gps} ,INS :{ins} , Kelman : {kal}")
+
+                #ال print بطبيعته بطئ فممكن ياخر في معالجه الداتا 
+                # print(f"Data from mostafa -> GPS : {gps} ,INS :{ins} , Kelman : {kal}")
                
                 
                 # إرجاع t للثواني
@@ -72,22 +78,27 @@ class Visualizer(QMainWindow):
                 self.gps_vals.append(gps)
                 self.ins_vals.append(ins)
                 self.kal_vals.append(kal)
-
+    
+                new_data = True
+                
                 # الحفاظ على آخر 200 نقطة للأداء
-                if len(self.times) > 200:
-                    self.times.pop(0)
-                    self.gps_vals.pop(0)
-                    self.ins_vals.pop(0)
-                    self.kal_vals.pop(0)
-            except:
+                # if len(self.times) > 200:
+                #     self.times.pop(0)
+                #     self.gps_vals.pop(0)
+                #     self.ins_vals.pop(0)
+                #     self.kal_vals.pop(0)
+            except queue.Empty:
+                break
+            except Exception as e:
+                print(f"Error parsing date: {e}")
                 break
 
         # تحديث الرسم
-        if self.times:
+        if new_date and len(self.item) > 0:
             self.ins_line.setData(self.times, self.ins_vals)
             self.gps_line.setData(self.times, self.gps_vals)
             self.kal_line.setData(self.times, self.kal_vals)
 
     def run(self):
         self.show()
-        app.exec_()
+        sys.exit(app.exec_())
